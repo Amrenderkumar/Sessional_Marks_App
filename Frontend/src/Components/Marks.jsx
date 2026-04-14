181b827d1bf1cc740050cd240c7a1fadff185b9d
Amrender kuamr <amrenderk66@gmail.com>
2026-04-14 20:30:08 +0530

    add new component

---
+++ Frontend/src/Components/Marks.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Marks = () => {
+  return (
+    <div>
+      <div className='bg-red-300 text-2xl'>
+        <h1>Marks</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Marks
