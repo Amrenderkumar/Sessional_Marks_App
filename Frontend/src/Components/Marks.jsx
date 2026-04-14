6337036d19b30bbab2e76429f72697f6cd49f68f
Amrender kuamr <amrenderk66@gmail.com>
2026-04-14 20:37:45 +0530

    update the css

--- Frontend/src/Components/Marks.jsx
+++ Frontend/src/Components/Marks.jsx
@@ -6,6 +6,15 @@ const Marks = () => {
       <div className='bg-red-300 text-2xl'>
         <h1>Marks</h1>
       </div>
+      <div>
+        <div className='bg-green-300'>
+          <h2>Student Name</h2>
+        </div>
+        <div className='bg-yellow-300'>
+          <h3>Subject</h3>
+          <p>Marks</p>
+        </div>
+      </div>
     </div>
   )
 }
